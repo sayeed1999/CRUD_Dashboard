c937737f89a52e67658057bceab18f71638b7167
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2022-06-11 18:03:57 +0600

    worked on ui design for crud dashboard

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -7,18 +7,31 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import Paper from '@mui/material/Paper';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+import Tooltip from '@mui/material/Tooltip';
 
 const Items = (props: any) => {
     return <Paper elevation={3} className="my-4">
         <List>
         {props.todos.map((todo: any) => (
             <ListItem disablePadding key={todo.id}>
-                <ListItemButton>
+                <ListItemButton disableTouchRipple style={{cursor: "default"}}>
                     <ListItemText primary={`${todo.id}. ${todo.title}`} />
+                    <Tooltip title="Modify">
+                      <IconButton color="primary">
+                        <EditIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Delete">
+                      <IconButton color="secondary">
+                        <DeleteIcon />
+                      </IconButton>
+                    </Tooltip>
                     <ListItemIcon>
                         <Checkbox
                           checked={!!todo.completed}
-                          disableRipple
                         />
                     </ListItemIcon>
                 </ListItemButton>
--- src/components/NewItem.tsx
+++ src/components/NewItem.tsx
@@ -5,9 +5,9 @@ const NewItem = () => {
     return <div className="row my-4">
         <div className="col-md-6">
             <div className='d-flex align-items-end'>
-                <TextField id="filled-basic" label="New Todo" variant="filled" className='flex-grow-1' />
+                <TextField id="filled-basic" label="New Todo here.." variant="filled" className='flex-grow-1' />
                 <span className='px-2'></span>
-                <Button variant="contained" size="large">Contained</Button>    
+                <Button variant="contained" size="large">Create New Todo</Button>    
             </div>
         </div>
     </div>;
